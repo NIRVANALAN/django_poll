0aa0d1312fa69b9ac28d7c8788a90e05baef0e2b
maver_MB13 <maverickers@outlook.com>
2019-06-14 10:56:34 +0800

    init

--- polls/models.py
+++ polls/models.py
@@ -2,24 +2,28 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Question(models.Model):
-    question_txt = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    # add descriptions in Question.objects.all()
-    def __str__(self):
-        return self.question_txt
-    
-    def was_published_recently(self):
-        '''
-        for show
-        '''
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	question_txt = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	
+	# add descriptions in Question.objects.all()
+	def __str__(self):
+		return self.question_txt
+	
+	def was_published_recently(self):
+		"""
+		for show
+		"""
+		return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question,on_delete = models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	
+	def __str__(self):
+		return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,63 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.template import loader
+from django.views import generic
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+	
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+	model = Question
+	template_name = 'polls/result.html'
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	print(latest_question_list[0].question_txt)
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': latest_question_list
+	}
+	# output = ','.join([q.question_text for q in latest_question_list])
+	# return HttpResponse("Hello, world. You're at the poll index")
+	# return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	# return HttpResponse("You 're looking at question {}".format(question_id))
+	return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+	response = "You are looking at the result of question {}".format(question_id)
+	return HttpResponse(response)
+
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except(KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': 'You did not select a choice'
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reversed())
